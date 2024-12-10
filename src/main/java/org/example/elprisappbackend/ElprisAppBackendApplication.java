3071d45a7e1c1f7c96c1e78ec1fa66996ed8300f
Adam <Adam.sturesson@live.se>
2024-12-10 15:02:36 +0100

    first commit

---
+++ src/main/java/org/example/elprisappbackend/ElprisAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.elprisappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElprisAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElprisAppBackendApplication.class, args);
+    }
+
+}
