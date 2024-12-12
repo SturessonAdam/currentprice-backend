4e05a71f4a4f73acc579281ee4dc258acfe9b11e
Adam <Adam.sturesson@live.se>
2024-12-12 10:40:39 +0100

    Updated TODO list

--- src/main/java/org/example/elprisappbackend/ElprisAppBackendApplication.java
+++ src/main/java/org/example/elprisappbackend/ElprisAppBackendApplication.java
@@ -3,6 +3,11 @@ package org.example.elprisappbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO
+// Cacha elrpis data
+// Köra API anrop på schema basis för att hålla elrprisenra uppdaterade
+// Berabeta datan innan den skickas till fronten
+
 @SpringBootApplication
 public class ElprisAppBackendApplication {
 
