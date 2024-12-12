2b88ac7d54e49ade9243285b245a1d6497cf1982
Adam <Adam.sturesson@live.se>
2024-12-12 10:35:32 +0100

    Added lombok for getter and setter

--- src/main/java/org/example/elprisappbackend/model/Model.java
+++ src/main/java/org/example/elprisappbackend/model/Model.java
@@ -1,7 +1,12 @@
 package org.example.elprisappbackend.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 public class Model {
 
     private double sekPerKWh;
@@ -9,45 +14,5 @@ public class Model {
     private double exr;
     private LocalDateTime timeStart;
     private LocalDateTime timeEnd;
-
-    public double getSekPerKWh() {
-        return sekPerKWh;
-    }
-
-    public void setSekPerKWh(double sekPerKWh) {
-        this.sekPerKWh = sekPerKWh;
-    }
-
-    public double getEurPerKWh() {
-        return eurPerKWh;
-    }
-
-    public void setEurPerKWh(double eurPerKWh) {
-        this.eurPerKWh = eurPerKWh;
-    }
-
-    public double getExr() {
-        return exr;
-    }
-
-    public void setExr(double exr) {
-        this.exr = exr;
-    }
-
-    public LocalDateTime getTimeStart() {
-        return timeStart;
-    }
-
-    public void setTimeStart(LocalDateTime timeStart) {
-        this.timeStart = timeStart;
-    }
-
-    public LocalDateTime getTimeEnd() {
-        return timeEnd;
-    }
-
-    public void setTimeEnd(LocalDateTime timeEnd) {
-        this.timeEnd = timeEnd;
-    }
 
 }
