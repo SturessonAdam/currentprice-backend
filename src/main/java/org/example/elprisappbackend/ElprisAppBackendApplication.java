107783377bb3f3e206c3964e439115cc7dd2b60e
Adam <Adam.sturesson@live.se>
2024-12-19 12:05:13 +0100

    updated todo list

--- src/main/java/org/example/elprisappbackend/ElprisAppBackendApplication.java
+++ src/main/java/org/example/elprisappbackend/ElprisAppBackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 // TODO
-// Cache för morgondagens priser?
 // Ta fram de 5 bästa timmarna på dygnet?
 // Historisk data
 
