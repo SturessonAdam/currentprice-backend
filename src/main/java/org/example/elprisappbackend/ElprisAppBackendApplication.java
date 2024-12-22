d7371b222b15d8817de5ab2837fadc88da021cfe
Adam <Adam.sturesson@live.se>
2024-12-22 10:04:34 +0100

    updated todo list

--- src/main/java/org/example/elprisappbackend/ElprisAppBackendApplication.java
+++ src/main/java/org/example/elprisappbackend/ElprisAppBackendApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 // TODO
 // Ta fram de 5 bästa timmarna på dygnet?
 // Historisk data
+// Bättre felhantering?
 
 @SpringBootApplication
 @EnableScheduling
