87666d04a632d1a63ae7380787b7d04a7b1809a1
Adam <Adam.sturesson@live.se>
2025-04-02 20:09:38 +0200

    Addec Cors

--- src/main/java/org/example/elprisappbackend/controller/Controller.java
+++ src/main/java/org/example/elprisappbackend/controller/Controller.java
@@ -2,10 +2,12 @@ package org.example.elprisappbackend.controller;
 
 import org.example.elprisappbackend.service.PowerpriceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class Controller {
 
